9a1d19be2841ac5197e15954c46de3b65cfc84e5
fDevJc <fdevjc@gmail.com>
2022-05-11 21:24:22 +0900

    [feat] 엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    //ORDINAL 쓰면 숫자로 들어가는데 운영중에 순서가 바뀌면 큰일난다.
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // [READY, COMP]
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    public Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    //member클래스의 member변수에 거울일 뿐이야
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
